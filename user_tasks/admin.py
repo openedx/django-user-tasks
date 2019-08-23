14d4b0dd051924bf754244a1ffe0b9bcfe52e5a5
Kyle McCormick <kmccormick@edx.org>
2019-08-23 15:35:09 -0400

    Improve list display for model admin (#22)
    * Improve list display for model admin
    
    * fixup! Inane documentation
    
    * fixup! Make the inane documentation gramatically correct
    
    * fixup! Waste vertical space
    
    * fixup! This blank line makes everything more readable

--- user_tasks/admin.py
+++ user_tasks/admin.py
@@ -9,5 +9,26 @@ from django.contrib import admin
 
 from .models import UserTaskArtifact, UserTaskStatus
 
-admin.site.register(UserTaskArtifact)
-admin.site.register(UserTaskStatus)
+
+class UserTaskArtifactAdmin(admin.ModelAdmin):
+    """
+    Configuration for UserTaskArtifact admin panel.
+    """
+
+    list_display = ('created', 'uuid', 'status', 'name', 'text')
+    ordering = ('created')
+    search_fields = ('uuid', 'name', 'text')
+
+
+class UserTaskStatusAdmin(admin.ModelAdmin):
+    """
+    Configuration for UserTaskStatus admin panel.
+    """
+
+    list_display = ('created', 'uuid', 'state', 'user', 'name')
+    ordering = ('created')
+    search_fields = ('uuid', 'task_id', 'task_class', 'user', 'name')
+
+
+admin.site.register(UserTaskArtifact, UserTaskArtifactAdmin)
+admin.site.register(UserTaskStatus, UserTaskStatusAdmin)
