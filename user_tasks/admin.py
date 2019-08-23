a8caf8055b2dfd3c88621d5a12474894a3cfdc20
Kyle McCormick <kmccormick@edx.org>
2019-08-23 15:15:38 -0400

    fixup! Make the inane documentation gramatically correct

--- user_tasks/admin.py
+++ user_tasks/admin.py
@@ -11,14 +11,14 @@ from .models import UserTaskArtifact, UserTaskStatus
 
 
 class UserTaskArtifactAdmin(admin.ModelAdmin):
-    """ Admin class UserTaskArtifact """
+    """ Configuration for UserTaskArtifact admin panel. """
     list_display = ('created', 'uuid', 'status', 'name', 'text')
     ordering = ('created')
     search_fields = ('uuid', 'name', 'text')
 
 
 class UserTaskStatusAdmin(admin.ModelAdmin):
-    """ Admin class UserTaskStatus """
+    """ Configuration for UserTaskStatus admin panel. """
     list_display = ('created', 'uuid', 'state', 'user', 'name')
     ordering = ('created')
     search_fields = ('uuid', 'task_id', 'task_class', 'user', 'name')
