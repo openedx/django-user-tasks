cd7aa396e18786647bd2b83768d18502bf309119
Soban Javed <iamsobanjaved@gmail.com>
2020-09-15 19:26:40 +0500

    Test task cancel logging

--- user_tasks/models.py
+++ user_tasks/models.py
@@ -136,15 +136,14 @@ class UserTaskStatus(TimeStampedModel):
             current_app.control.revoke(self.task_id)
             user_task_stopped.send_robust(UserTaskStatus, status=self)
         try:
-            with transaction.atomic():
-                status = UserTaskStatus.objects.select_for_update().get(pk=self.id)
-                if status.state in (UserTaskStatus.CANCELED, UserTaskStatus.FAILED, UserTaskStatus.SUCCEEDED):
-                    return
-                status.state = UserTaskStatus.CANCELED
-                status.save(update_fields={'state', 'modified'})
-                self.state = status.state
-                self.modified = status.modified
-                LOGGER.info("Canceled task %s" % status.task_id)
+            status = UserTaskStatus.objects.select_for_update().get(pk=self.id)
+            if status.state in (UserTaskStatus.CANCELED, UserTaskStatus.FAILED, UserTaskStatus.SUCCEEDED):
+                return
+            status.state = UserTaskStatus.CANCELED
+            status.save(update_fields={'state', 'modified'})
+            self.state = status.state
+            self.modified = status.modified
+            LOGGER.info("Canceled task %s" % status.task_id)
         except Exception as exc:
             LOGGER.info("Cancellation of task %s is failed with following exception: %s" % (status.task_id, exc))
 
