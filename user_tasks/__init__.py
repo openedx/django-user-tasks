07b42ddcc4032a560259c6710cc55daea9588ca6
=Awais Jibran <awaisdar001@gmail.com>
2021-06-07 17:54:15 +0500

    perf: address django admin timeout issue

--- user_tasks/__init__.py
+++ user_tasks/__init__.py
@@ -4,7 +4,7 @@ Management of user-triggered asynchronous tasks in Django projects.
 
 from django.dispatch import Signal
 
-__version__ = '2.0.2'
+__version__ = '2.0.3'
 
 default_app_config = 'user_tasks.apps.UserTasksConfig'  # pylint: disable=invalid-name
 
