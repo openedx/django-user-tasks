5567cdaf4856977ea9f1421e27029b76c519f5e1
GonzaloRomero <gonzalo.romero@edunext.co>
2021-10-05 17:36:21 -0500

    Move django rest swagger to drf-yasg

--- schema/urls.py
+++ schema/urls.py
@@ -5,12 +5,31 @@ URLs for REST API schema generation.
 from django.conf.urls import include, url
 from django.contrib import admin
 
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+from rest_framework import permissions
+
 from user_tasks.urls import urlpatterns as base_patterns
 
 from .views import swagger
 
+schema_view = get_schema_view(
+   openapi.Info(
+      title="Snippets API",
+      default_version='v1',
+      description="Test description",
+      terms_of_service="https://www.google.com/policies/terms/",
+      contact=openapi.Contact(email="contact@snippets.local"),
+      license=openapi.License(name="BSD License"),
+   ),
+   public=True,
+   permission_classes=[permissions.AllowAny],
+)
+
 # The Swagger/Open API JSON file can be found at http://localhost:8000/?format=openapi
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
-    url('^$', swagger),
+    url(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
+    url(r'^swagger/$', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    url(r'^redoc/$', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ] + base_patterns
