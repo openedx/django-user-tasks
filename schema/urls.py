6c9ee2df5e00f337d31849dad7ca37efe6c3d83f
GonzaloRomero <gonzalo.romero@edunext.co>
2021-10-12 11:42:22 -0500

    chore: switch to drf-yasg
    feat: replace rest_framework_swagger with drf_yasg
    
    feat: resolve conflicted dependencies
    
    feat: resolve doc.txt file conflicted dependencies
    
    feat: resolve doc.txt file conflicted dependencies_02
    
    feat: resolve doc.txt file conflicted dependencies_03
    
    feat: resolve doc.txt file conflicted dependencies_04
    
    feat: resolve conflicted files to master
    
    feat: resolve conflicted files to master branch
    
    feat: solve quality tests and conflicted files
    
    feat: solve conflicted files

--- schema/urls.py
+++ schema/urls.py
@@ -5,12 +5,25 @@ URLs for REST API schema generation.
 from django.conf.urls import include, url
 from django.contrib import admin
 
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+from rest_framework import permissions
+
 from user_tasks.urls import urlpatterns as base_patterns
 
-from .views import swagger
+SCHEMA = get_schema_view(
+   openapi.Info(
+      title="Django User Tasks API",
+      default_version='v1',
+      description="Django User Tasks API docs",
+   ),
+   public=False,
+   permission_classes=[permissions.AllowAny],
+)
 
 # The Swagger/Open API JSON file can be found at http://localhost:8000/?format=openapi
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
-    url('^$', swagger),
+    url(r'^swagger(?P<format>\.json|\.yaml)$', SCHEMA.without_ui(cache_timeout=0), name='schema-json'),
+    url(r'^swagger/$', SCHEMA.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
 ] + base_patterns
