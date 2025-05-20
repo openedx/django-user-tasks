132e8469a5d0eefdfadfabffd03c74db63a71401
awais qureshi <awais.qureshi@arbisoft.com>
2025-05-20 18:10:33 +0500

    fix: Fixing `get_storage` functionality as per new django52.

--- tests/test_storages.py
+++ tests/test_storages.py
@@ -1,3 +1,4 @@
+""" Storages tests."""
 from storages.backends.s3boto3 import S3Boto3Storage
 
 from django.core.files.storage import FileSystemStorage
@@ -8,7 +9,7 @@ from user_tasks.conf import settings as user_tasks_settings
 
 
 class TestUserTaskStatus(TestCase):
-
+    """ Test cases for storages."""
     @override_settings(
         USER_TASKS_ARTIFACT_STORAGE="storages.backends.s3boto3.S3Boto3Storage"
     )
@@ -36,6 +37,7 @@ class TestUserTaskStatus(TestCase):
         Since LazySettings does not expose STORAGES from Django settings directly,
         we monkey-patch it on the user_tasks_settings object.
         """
+        # pylint: disable=literal-used-as-attribute
         setattr(user_tasks_settings, 'STORAGES', {
             "default": {
                 "BACKEND": "django.core.files.storage.FileSystemStorage",
