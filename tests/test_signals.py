24a3da3df130c36629c83d96be2e8c62b34585ec
asajjad2 <areebsajjad21@gmail.com>
2025-06-24 13:59:44 +0500

    chore: reorder imports in test_signals.py to satisfy isort

--- tests/test_signals.py
+++ tests/test_signals.py
@@ -18,9 +18,9 @@ from django.test import TestCase, TransactionTestCase, override_settings
 
 from user_tasks import user_task_stopped
 from user_tasks.models import UserTaskStatus
-from user_tasks.signals import start_user_task, celery_app, create_user_task
+from user_tasks.signals import celery_app, create_user_task, start_user_task
+from user_tasks.utils import extract_proto2_embed, extract_proto2_headers, proto2_to_proto1
 from user_tasks.tasks import UserTask
-from user_tasks.utils import proto2_to_proto1, extract_proto2_headers, extract_proto2_embed
 
 User = auth.get_user_model()
 
